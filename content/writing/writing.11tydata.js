5d6a46b99f9275082c1c94d4ba0fb36c825c235e
gdewalters <gareth@dewalters.net>
2025-07-16 07:11:56 +1200

    Update writing.11tydata.js
    You're getting an extra, unwanted HTML file (likely an empty or partially rendered article.html or index.html within an article folder) that corresponds directly to your article.njk template file, in addition to the correctly generated individual article pages.
    
    This is a common scenario when a template file (like article.njk) is used for pagination, but Eleventy also tries to render it as a standalone page. Our current writing.11tydata.js is supposed to prevent this by setting permalink: false when data.post isn't defined, but it seems to not be working in this specific case.
    
    Let's add some more explicit logging to your writing.11tydata.js to truly understand what the permalink function sees when it processes article.njk as a standalone file, and to ensure we're correctly accessing the slug from your Contentful data structure (a point of confusion earlier).

--- content/writing/writing.11tydata.js
+++ content/writing/writing.11tydata.js
@@ -10,14 +10,34 @@ export default {
   // All files in /content/writing/ to default to this layout
   layout: "layouts/article.njk",
   // Compute permalink from each article slug
+
+
   eleventyComputed: {
     permalink: (data) => {
-      if (data.post && data.post.slug) {
-        return `/writing/${data.post.slug}/index.html`;
+      console.log('--- DEBUG: Permalink function triggered for template:', data.page?.inputPath);
+      console.log('  Is data.post defined?', !!data.post); // Log if data.post exists
+      if (data.post) {
+        // Attempt to get slug from 'fields' first, then direct 'post' object
+        const articleSlug = data.post.fields?.slug || data.post.slug;
+        console.log('  Attempted slug (data.post.fields?.slug || data.post.slug):', articleSlug);
+
+        if (articleSlug) {
+          const permalinkPath = `/writing/${articleSlug}/index.html`;
+          console.log('  Permalink returned:', permalinkPath);
+          return permalinkPath;
+        } else {
+          console.log('  Permalink returned: false (article slug not found)');
+          return false; // Slug is missing for a paginated item
+        }
+      } else {
+        // This branch should be hit when article.njk is processed as a standalone file
+        console.log('  Permalink returned: false (data.post is undefined, likely standalone template)');
+        return false; // data.post is undefined, so don't create a page
       }
-      return false;
     },
   },
+
+
   // If you want all files in /content/writing/ to have this tag
   tags: [
     "writing",
