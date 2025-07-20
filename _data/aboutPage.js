b872f618f760f74810778b3bb9fbe58c357bdf26
gdewalters <gareth@dewalters.net>
2025-07-20 23:11:04 +1200

    Add debug JSON dumps and console output

--- _data/aboutPage.js
+++ _data/aboutPage.js
@@ -2,5 +2,7 @@ import getContentfulPages from './getContentfulPages.js';
 
 export default async function () {
   const pages = await getContentfulPages();
-  return pages.find((p) => p.sys && p.sys.id === '6Q96jcdFbNmzlFw6rkjuI3');
+  const page = pages.find((p) => p.sys && p.sys.id === '6Q96jcdFbNmzlFw6rkjuI3');
+  console.debug('About page data:', page);
+  return page;
 }
