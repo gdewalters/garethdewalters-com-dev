2be739195732dcf63a26dc3df06bdee727a0078c
gdewalters <gareth@dewalters.net>
2025-07-24 00:17:57 +1200

    tidy up scripts

--- _helpers/build-js.js
+++ _helpers/build-js.js
@@ -5,7 +5,7 @@ import { build } from 'esbuild';
 import { join } from 'path';
 
 // Path to the main browser script.
-const entry = join('src', 'scripts', 'main.js');
+const entry = join('scripts', 'main.js');
 // Destination for the bundled file that will be loaded by the site.
 const outfile = join('public', 'assets', 'scripts', 'bundle.js');
 
