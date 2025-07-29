479a4861beaf73e3d3e1b15ce086d79431034f2f
gdewalters <gareth@dewalters.net>
2025-07-29 20:51:50 +1200

    Add helper to fetch pages by slug

--- _data/getContentfulPageAbout.js
+++ _data/getContentfulPageAbout.js
@@ -1,7 +1,6 @@
-import getContentfulPages from './getContentfulPages.js';
+import { getContentfulPageBySlug } from './getContentfulPageBySlug.js';
 
 export default async function () {
   // See _data/models/contentfulPageEntryAbout.json for a sample entry shape.
-  const pages = await getContentfulPages();
-  return pages.find((p) => p.sys && p.sys.id === '6Q96jcdFbNmzlFw6rkjuI3');
+  return getContentfulPageBySlug('about');
 }
