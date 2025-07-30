3767440fe5525eb93a1c79e2517354a9aa1ed651
gdewalters <gareth@dewalters.net>
2025-07-30 19:45:31 +1200

    Update metadata.js

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,10 @@
 export default {
 	title: "Gareth de Walters",
 	url: "https://garethdewalters.com/",
-	language: "en",
+	language: "en-nz",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
 		name: "Gareth de Walters",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		url: "https://garethdewalters.com/about/"
 	}
 }
